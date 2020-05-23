1888014dbefb3c42469b4191cdb0dce48c100b1c
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-05-23 20:57:44 -0300

    chore: using Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => (
-    <h1>About Page</h1>
+    <>
+      <h1>About Page</h1>
+      <ul>
+      <li>
+          <Link
+            to="/"
+          >
+          Home
+          </Link>
+      </li>
+      <li>
+          <a href="/about">About</a>
+      </li>
+      <li>
+          <Link
+						to="/about"
+						activeStyle={{ color: "red" }}
+					>
+					About (Gatsby)
+					</Link>
+      </li>
+    </ul>
+    </>
 )
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,22 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+          <Link
+            to="/"
+            activeStyle={{ color: "red" }}
+          >
+          Home
+          </Link>
+      </li>
+      <li>
+          <a href="/about">About</a>
+      </li>
+      <li>
+          <Link to="/about">About (Gatsby)</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
