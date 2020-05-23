44eea05a54937f0af845a557cbb0ea5cc02e8a14
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-05-23 20:45:08 -0300

    chore: create about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My blog`,
+    description: `Blog og the development.`,
+    author: `@gmarquinhusgonc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
