6c36c12b7e219b1094cf73d3f537aedc9f427544
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-05-31 18:07:49 -0300

    chore: add serach with algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config()
+
+const queries = require("./src/utils/algolia_queries")
+
 module.exports = {
   siteMetadata: {
     title: `Marquinhus Gonçalves`,
@@ -54,6 +58,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: false,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
