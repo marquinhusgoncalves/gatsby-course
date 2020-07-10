b7ba5808ccee8520c51e0c07e4f5149d93e70a96
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-07-10 20:34:58 -0300

    fix typo SEO

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import SEO from "../components/Seo"
 import RecommendedPost from "../components/RecommendedPost"
 import Comments from "../components/Comments"
 
